1ff703e6ffbfbb6e2044e5871ff283ce6758a1aa
Ban049 <banyou049@gmail.com>
2026-04-12 22:56:39 +1000

    Admin Создание метода для чтения логов

--- Cursah/Cursah/Program.cs
+++ Cursah/Cursah/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+static void ViewLogs(Dictionary<string, string> args)
+{
+    int limit = int.TryParse(args.GetValueOrDefault("n", "5"), out var val) ? val : 5;
+    using var conn = new SqlConnection(ConnStr);
+    conn.Open();
+    var cmd = new SqlCommand("SELECT TOP(@n) CreatedAt, Level, Message FROM SystemLogs ORDER BY CreatedAt DESC", conn);
+    cmd.Parameters.AddWithValue("@n", limit);
+    using var r = cmd.ExecuteReader();
+    while (r.Read()) Console.WriteLine($"{r.GetDateTime(0):T} [{r.GetString(1)}] {r.GetString(2)}");
+}
